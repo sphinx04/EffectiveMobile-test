a9cce89bc235b766d443f750f036df294700ad95
Mnatsakan Zurnadzhian <mnats1997@gmail.com>
2023-03-16 13:53:28 +0400

    Initial Commit

---
+++ EffectiveMobile-test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EffectiveMobile-test
+//
+//  Created by Mnatsakan Zurnadzhian on 16.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EffectiveMobile-test/EffectiveMobile_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobile_testApp.swift
+//  EffectiveMobile-test
+//
+//  Created by Mnatsakan Zurnadzhian on 16.03.23.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobile_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
